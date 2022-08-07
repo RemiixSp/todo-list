dcb518d8f2c34bec0ceabe32198b1bb8d1d3dcee
remiixsp <Battlenet22>
2022-08-07 15:50:08 +0300

    style: :lipstick: adding ip widget block structure

--- src/ui/components/ipFinder/index.tsx
+++ src/ui/components/ipFinder/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Button from '../../common/button';
 import styles from './ip.module.scss';
 
 const ipFinder = () => {
-  return <div className={styles.apiWidget}>ipFinder</div>;
+  return (
+    <div className={styles.apiWidget}>
+      <h3 className={styles.ipHeader}>Find by ip</h3>
+      <div className={styles.foundInfo}></div>
+      <div className={styles.getIpContainer}>
+        <input
+          className={styles.getIpField}
+          placeholder='Enter ip'
+          type='text'
+        />
+
+        <Button onClick={() => {}} className={styles.submitIp}>
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ipFinder;
