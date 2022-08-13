e79cf2d1f210dfa83b6d18231696bd5c6a60a5ba
remiixsp <Battlenet22>
2022-08-13 18:39:30 +0300

    style: :lipstick: login dark mode screen

--- src/ui/views/authorization/index.tsx
+++ src/ui/views/authorization/index.tsx
@@ -5,8 +5,11 @@ import cn from 'classnames';
 import { login } from '../../../core/store/authorization';
 import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
+import { useAppSelector } from '../../../core/store/store';
 
 const AuthorizationView = () => {
+  const darkTheme = useAppSelector((state) => state.theme.darkMode);
+
   const dispatch = useDispatch();
 
   const initialValues = {
@@ -46,7 +49,11 @@ const AuthorizationView = () => {
         <div className='container py-5 h-100'>
           <div className='row d-flex justify-content-center align-items-center h-100'>
             <div className='col-12 col-md-8 col-lg-6 col-xl-5'>
-              <div className='card shadow-2-strong'>
+              <div
+                className={cn('card shadow-2-strong', {
+                  [styles.darkThemeContainer]: darkTheme,
+                })}
+              >
                 <div className='card-body p-5 text-center'>
                   <h3 className='mb-5'>Sign in</h3>
 
